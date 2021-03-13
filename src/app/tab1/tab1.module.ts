02692aaef6beb15760994b0ad99ff00a8e822c4b
Adnan Aheer <adnanaheer20@gmail.com>
2021-03-12 23:49:50 -0800

    import footer component in tab1

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { HomeViewComponent } from '../home/home-view.component';
+import { FooterComponent } from '../footer/footer.component';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -16,6 +17,10 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page,HomeViewComponent]
+  declarations: [
+    Tab1Page,
+    HomeViewComponent,
+    FooterComponent
+  ]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule { }
